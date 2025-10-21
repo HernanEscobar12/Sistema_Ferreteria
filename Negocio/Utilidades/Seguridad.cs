9610db4bf2101518b982ea24f8167c8fb7088a2c
Hernan Escobar <guidohernanescobar@gmail.com>
2025-10-21 18:51:21 -0300

    agrego Utilidades en la capa negocio

---
+++ Negocio/Utilidades/Seguridad.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio.Utilidades
+{
+    public class Seguridad
+    {
+        public static string Encriptar(string texto)
+        {
+            SHA256 sHA256 = SHA256.Create();
+
+            if(string.IsNullOrEmpty(texto))
+                throw new ArgumentNullException(nameof(texto));
+
+            byte[] bytes = Encoding.UTF8.GetBytes(texto);
+            byte[] hash = sHA256.ComputeHash(bytes);
+
+            StringBuilder Sb = new StringBuilder();
+
+            foreach (byte b in hash)
+                Sb.Append(b.ToString("x2"));
+            
+            return Sb.ToString();
+
+        }
+
+
+    }
+}
