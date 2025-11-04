2d9ee4d4f5122484d63b4e78b555db013c691a48
elFedaEZ <albornozfederico98@hotmail.com>
2025-11-04 16:58:21 -0300

    Agregar fix CapaDominio-Inventario

--- Dominio/Inventario.cs
+++ Dominio/Inventario.cs
@@ -8,20 +8,16 @@ namespace Dominio
 {
     public class Inventario
     {
-        public int InventarioId { get; set; }   
-
+        public int InventarioId { get; set; }
         public Producto Producto { get; set; }
-
-        public int StockActual { get; set; }    
+        public int StockActual { get; set; }
         public int StockMinimo { get; set; }
-
-        public string Ubicacion { get; set; }  // 🔹 NUEVO
+        public string Ubicacion { get; set; }
         public Sucursal Sucursal { get; set; }
-
-        public bool EsCritico => StockActual <= StockMinimo;
-
-        // 👇 Esta propiedad te deja mostrar el nombre del producto directo
-        //public string NombreProducto => Producto?.Nombre ?? "(Sin nombre)";
         public DateTime UltimaActualizacion { get; set; }
+
+        // 🔹 Propiedad calculada: no se asigna manualmente
+        public bool EsCritico { get; set; }
+
     }
 }
